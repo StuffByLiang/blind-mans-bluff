6a222d3d2d3c121f09df5e5814738b4a1ea62c90
Liang Liu <stuffbyliang@gmail.com>
2024-08-24 23:00:18 -0500

    images

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import logging
-from flask import Flask, request
+from flask import Flask, request, send_from_directory
 from evaluator import Evaluator
 from threading import Thread
 import os
@@ -15,21 +15,37 @@ if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-# create a logger that is of INFO that only logs to a file
-logger = logging.getLogger("Evaluator")
-if not os.path.exists('logs'):
-    os.makedirs('logs')
-handler = logging.FileHandler('logs/evaluator.log', mode='w+')
-handler.setFormatter(logging.Formatter('%(message)s'))
-handler.setLevel(logging.INFO)
-logger.addHandler(handler)
-logger.propagate = False
 
-evaluator = Evaluator(logger)
-def run_evaluator():
-    evaluator.reload_strategies()
-    evaluator.evaluate_strategies()
-Thread(target=run_evaluator).start()
+class EvaluatorWrapper():
+    def __init__(self):
+        super().__init__()
+        self.thread = None
+        self.evaluator = None
+    
+    def run(self):
+        # create a logger that is of INFO that only logs to a file
+        logger = logging.getLogger("Evaluator")
+        if not os.path.exists('logs'):
+            os.makedirs('logs')
+        handler = logging.FileHandler('logs/evaluator.log', mode='w+')
+        handler.setFormatter(logging.Formatter('%(message)s'))
+        handler.setLevel(logging.INFO)
+        logger.addHandler(handler)
+        logger.propagate = False
+        self.evaluator = Evaluator(logger)
+        self.evaluator.load_strategies()
+
+        thread = Thread(target=self.evaluator.evaluate_strategies)
+        thread.start()
+
+    def restart(self):
+        if self.thread:
+            evaluator_wrapper.stop = True
+            self.thread.join()
+        self.run()
+
+evaluator_wrapper = EvaluatorWrapper()
+evaluator_wrapper.restart()
 
 ########## Helpers ##########
 
@@ -40,7 +56,7 @@ def allowed_file(filename):
 
 @app.route("/")
 def hello_world():
-    strategies = evaluator.strategies
+    strategies = evaluator_wrapper.evaluator.strategies
     return f"""
     <h1>Indian Poker Strategy Evaluator</h1>
     <p>Upload your strategies to see how they perform against each other</p>
@@ -53,9 +69,27 @@ def hello_world():
 def results():
     try:
         with open('results/results.txt', 'r') as f:
-            return f.read()
+            # read the last 3 lines
+            lines = f.readlines()[-3:]
+
+            results = "<h1>Results</h1>"
+            for line in lines:
+                results += f"<p>{line}</p>"
+
+            # now get all pngs in results folder and display them
+            images = ""
+            for file in os.listdir('results'):
+                if file.endswith('.png'):
+                    images += f'<img src="results/{file}" width="500" />'
+                
+            return results + images
     except FileNotFoundError:
         return "No results yet"
+    
+# add results as a public folder
+@app.route('/results/<path:path>')
+def send_results(path):
+    return send_from_directory('results', path)
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
@@ -74,7 +108,7 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = file.filename
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            evaluator.reload_strategies()
+            evaluator_wrapper.restart()
             return 'File successfully uploaded'
     
     return '''
--- evaluator.py
+++ evaluator.py
@@ -1,6 +1,7 @@
 # import all strategies from strategies folder and play mulitple rounds of indian poker
 from indianpoker import Strategy, IndianPokerGame, simulate_game
 from threading import Lock
+from pathlib import Path
 import os
 import logging
 import datetime
@@ -9,6 +10,7 @@ import matplotlib
 matplotlib.use('agg')
 import itertools
 
+RESULTS_DIR = Path('results')
 
 class Evaluator:
     def __init__(self, logger = logging.getLogger(__name__)):
@@ -17,32 +19,33 @@ class Evaluator:
         self.number_of_chips = {}
         self.number_of_games = {}
         self.lock = Lock()
+        self.stop = False
+
+        # remove all files from results folder and make it if needed
+        if not RESULTS_DIR.exists():
+            RESULTS_DIR.mkdir()
+            for file in RESULTS_DIR.iterdir():
+                file.unlink()
 
     def load_strategies(self):
-        strategies = {}
+        self.strategies = {}
         for file in os.listdir('strategies'):
             if file.endswith('.py') and file != 'indianpoker.py':
                 try:
                     strategy_module = __import__(f'strategies.{file[:-3]}', fromlist=[''])
                     strategy_class: Strategy = getattr(strategy_module, "strategy")
-                    strategies[strategy_class.player_id] = strategy_class()
+                    self.strategies[strategy_class.player_id] = strategy_class()
+                    self.logger.info(f"Loaded strategy {strategy_class.player_id} from file {file})")
                 except Exception as e:
                     self.logger.exception(f"Error loading strategy from file {file}")
-        return strategies
 
-    def reload_strategies(self):
-        with self.lock:
-            # pitch the strategies against each other
-            self.strategies = self.load_strategies()
-            self.logger.info(f"Reloaded Strategies: strategies playing: {self.strategies}")
-
-            self.number_of_chips = {
-                strategy: 0 for strategy in self.strategies
-            }
-            self.number_of_games = {
-                strategy: 0 for strategy in self.strategies
-            }
-
+        self.number_of_chips = {
+            strategy: 0 for strategy in self.strategies
+        }
+        self.number_of_games = {
+            strategy: 0 for strategy in self.strategies
+        }
+        self.logger.info(f"Reloaded Strategies: strategies playing: {self.strategies}")
 
     def evaluate_strategies(self):
         ante=5
@@ -53,12 +56,15 @@ class Evaluator:
 
         last_log_game = 0
         log_every = 100
+        last_write_num = 0
 
         while True:
             with self.lock:
                 if len(self.strategies) < 3:
                     continue
                 for strategies in itertools.combinations(self.strategies, 3):
+                    if self.stop:
+                        return
                     game = simulate_game({k: v for k, v in self.strategies.items() if k in strategies}, ante, starting_stack, rounds)
 
                     for strategy in strategies:
@@ -74,7 +80,8 @@ class Evaluator:
 
                 if number_of_games > last_log_game + log_every:
                     last_log_game += log_every
-                    print(f"Average Stack Sizes: {avg_stack_size}")
+                    self.logger.info(f"Average Stack Sizes: {avg_stack_size}")
+
                 if datetime.datetime.now() - last_write_time > datetime.timedelta(seconds=1):
                     with open('results/results.txt', 'a') as f:
                         results_json = {
@@ -93,11 +100,12 @@ class Evaluator:
                     for k,v in size_by_player.items():
                         plt.plot( list(range(num_rounds_used)), v, label=k )
                     plt.legend()
-                    plt.savefig('results/results.png')
+                    plt.savefig(f'results/results{last_write_num % 10}.png')
                     plt.clf()
+                    last_write_num += 1
                     last_write_time = datetime.datetime.now()
 
 if __name__ == "__main__":
     evaluator = Evaluator()
-    evaluator.reload_strategies()
-    strategies = evaluator.evaluate_strategies()
+    evaluator.load_strategies()
+    evaluator.evaluate_strategies()
